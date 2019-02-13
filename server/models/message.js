0c01056bd5518f90f25599f9c81b01b2c8e91291
Riku Kinnunen <riku.kinnunen@eng.tamk.fi>
2019-02-13 21:07:57 +0200

    Add handlers and routes for messages

--- server/models/message.js
+++ server/models/message.js
@@ -1,22 +1,27 @@
 const mongoose = require("mongoose");
 const User = require("./user");
 
-const messageSchema = new mongoose.Schema({
-  text: {
-    type: String,
-    required: true,
-    maxLength: 160
+const messageSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+      maxLength: 160
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
   },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
+  {
+    timestamps: true
   }
-});
+);
 
 messageSchema.pre("remove", async function(next) {
   //find user, remove message from user's messages array, save user
   try {
-    let user = await User.findById(this.userId);
+    let user = await User.findById(this.user);
     user.message.remove(this.id);
     await user.save();
     return next();
--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const { createMessage } = require("../handlers/messages");
+const {
+  createMessage,
+  getMessage,
+  deleteMessage
+} = require("../handlers/messages");
 
+//prefix with /api/user/:id/messages/
 router.route("/").post(createMessage);
+
+router
+  .route("/:message_id")
+  .get(getMessage)
+  .delete(deleteMessage);
 
 module.exports = router;
