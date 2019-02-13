7fbd2878ba4734f0ad2d9c1818bdcdc4ef0ecb31
Riku Kinnunen <riku.kinnunen@eng.tamk.fi>
2019-02-13 20:24:20 +0200

    Add authentication middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,26 @@
+require("dotenv").load();
+const jwt = require("jsonwebtoken");
+
+//check if logged in (Authentication)
+exports.loginRequired = function(req, res, next) {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    jwt.verify(token, process.env.SECRET_KEY, function(err, decoded) {
+      if (decoded) {
+        return next();
+      } else {
+        return next({
+          status: 401,
+          message: "Please login first"
+        });
+      }
+    });
+  } catch (err) {
+    return next({
+      status: 401,
+      message: "Please login first"
+    });
+  }
+};
+
+//check if right user (Authorization)
