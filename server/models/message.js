72047221dfb32cca3d49d1077d26118e19a95ba8
Riku Kinnunen <riku.kinnunen@eng.tamk.fi>
2019-02-13 19:31:40 +0200

    Add message model and connect it to user

---
+++ server/models/message.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const User = require("./user");
+
+const messageSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+    maxLength: 160
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
+});
+
+messageSchema.pre("remove", async function(next) {
+  //find user, remove message from user's messages array, save user
+  try {
+    let user = await User.findById(this.userId);
+    user.message.remove(this.id);
+    await user.save();
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+const Message = mongoose.model("Message", messageSchema);
+
+module.exports = Message;
