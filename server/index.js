c117512aab2c2ff771dce3d575d8f74a9f0bb528
Riku Kinnunen <riku.kinnunen@eng.tamk.fi>
2019-02-13 19:46:20 +0200

    Add createMessage functionality

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const errorHandler = require("./handlers/error");
 const authRoutes = require("./routes/auth");
+const messagesRoutes = require("./routes/messages");
 
 const PORT = 8080;
 
@@ -13,6 +14,7 @@ app.use(bodyParser.json());
 
 //routes go here
 app.use("/api/auth", authRoutes);
+app.use("/api/users/:id/messages", messagesRoutes);
 
 app.use(function(req, res, next) {
   let err = new Error("Not Found");
