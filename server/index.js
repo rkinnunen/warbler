6682494d7c51d9255596ae8f926e56d865887551
Riku Kinnunen <riku.kinnunen@eng.tamk.fi>
2019-02-13 15:59:49 +0200

    Add simple errorhandler

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const errorHandler = require("./handlers/error");
 
 const PORT = 8080;
 
@@ -15,6 +16,8 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, function() {
   console.log(`Server is starting on port ${PORT}`);
