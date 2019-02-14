12c911f928ebf41b8803222c63cb6dc2b423d15f
Riku Kinnunen <riku.kinnunen@eng.tamk.fi>
2019-02-14 15:09:21 +0200

    Add AuthForm component

--- client/src/containers/Main.js
+++ client/src/containers/Main.js
@@ -2,12 +2,40 @@ import React from "react";
 import { Switch, Route, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import Homepage from "../components/Homepage";
+import AuthForm from "../components/AuthForm";
 
 const Main = props => {
   return (
     <div className="container">
       <Switch>
         <Route exact path="/" render={props => <Homepage {...props} />} />
+        <Route
+          exact
+          path="/signin"
+          render={props => {
+            return (
+              <AuthForm
+                buttonText="Log In"
+                heading="Welcome Back!"
+                {...props}
+              />
+            );
+          }}
+        />
+        <Route
+          exact
+          path="/signup"
+          render={props => {
+            return (
+              <AuthForm
+                signUp
+                buttonText="Sign Up!"
+                heading="Join Today!"
+                {...props}
+              />
+            );
+          }}
+        />
       </Switch>
     </div>
   );
