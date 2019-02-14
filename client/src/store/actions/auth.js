89c0c25617e0ad39ba6b2f61258871aab89b0726
Riku Kinnunen <riku.kinnunen@eng.tamk.fi>
2019-02-14 18:55:14 +0200

    Add JWT token into headers in message api calls

--- client/src/store/actions/auth.js
+++ client/src/store/actions/auth.js
@@ -1,4 +1,4 @@
-import { apiCall } from "../../services/api";
+import { apiCall, setTokenHeader } from "../../services/api";
 import { SET_CURRENT_USER } from "../actionTypes";
 import { addError, removeError } from "./errors";
 
@@ -8,12 +8,18 @@ export function setCurrentUser(user) {
     user
   };
 }
+
+export function setAuthorizationToken(token) {
+  setTokenHeader(token);
+}
+
 export function authUser(type, userData) {
   return dispatch => {
     return new Promise((resolve, reject) => {
       return apiCall("post", `/api/auth/${type}`, userData)
         .then(({ token, ...user }) => {
           localStorage.setItem("jwtToken", token);
+          setAuthorizationToken(token);
           dispatch(setCurrentUser(user));
           dispatch(removeError());
           resolve();
@@ -29,6 +35,7 @@ export function authUser(type, userData) {
 export function logout() {
   return dispatch => {
     localStorage.clear();
+    setAuthorizationToken(false);
     dispatch(setCurrentUser({}));
   };
 }
